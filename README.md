edf9e663a6e8d29f889129615ba5026598897729
Sean Meade <seanpatmeade@gmail.com>
2021-01-08 23:37:35 +0000

    Final changes before deadline. References were add, notebook ran top to bottom, app tested, added README, and few minor changes.

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# MLandStats-Project
+# Machine Learning and Statistics - Project
+
+This repository contains my submission for Machine Learning and Statistics, a module as part of the H.Dip in data analytics with GMIT (2020).
+
+The file *trainingModel.ipynb* contains an explaination of the data set found in *powerproduction.csv*, attempts at training two models to predict the power output for a given wind speed, and an alysis of their accuracy.
+
+The *Dockerfile* file is used to implement a Dockker container utilizing *.dockerignore* to ignore unnecessary files for implementation.
+
+*powerproduction.py* contains a Flask app that uses the model developed in *trainingModel.ipynb* and can be used to create a web locally (as shown below) or can be used by the *Dockerfile*.
+
+*requirements.txt* contains all the packages required to run *powerproduction.py*.
+
+The *imgs* folder contains images used for explaination in *trainingModel.ipynb* and the *static* folder contains *index.html*.
+
+*index.html* is the template used by the Flask app *powerproduction.py*.
+
+*ProjectPDF.pdf* contains the instructions for the project given by lecture Ian McLoughlin.
+
+## Implementing power production prediction app
+---
+### Windows
+cd into working directory and then copy and paste the following command:
+#### Flask
+```
+python powerproduction.py
+```
+#### Docker
+```
+docker build . -t powerproduction-app .
+docker run -d -p 5000:5000 rando-image
+```
+
+### Linux
+```
+export FLASK_APP=powerproduction.py
+python3 -m flask run
+```
--- powerproduction.py
+++ powerproduction.py
@@ -3,6 +3,7 @@ import csv
 import numpy as np
 import pandas as pd
 from sklearn.neighbors import RadiusNeighborsRegressor
+import requests
 
 app = fl.Flask(__name__)
 
@@ -10,27 +11,31 @@ app = fl.Flask(__name__)
 def home():
     return app.send_static_file("index.html")
 
-@app.route("/powerproduction")
+@app.route("/powerproduction", methods=["POST"])
 def powerproduction():
-    # import csv data and convert to pandas dataframe
-    df = pd.read_csv("powerproduction.csv")
+    if fl.request.method == "POST":
+        speed = {}
+        speed = float(fl.request.form['speed'])
+        # speed = requests.get(data['input_s'])
+        # import csv data and convert to pandas dataframe
+        df = pd.read_csv("powerproduction.csv")
 
-    # remove all zeros
-    df = df[df.power != 0]
+        # remove all zeros
+        df = df[df.power != 0]
 
-    # put rows in order of speed
-    df = df.sort_values('speed')
+        # put rows in order of speed
+        df = df.sort_values('speed')
 
-    # set each column to a numpy array for processing
-    S = df['speed'].to_numpy()
-    p = df['power'].to_numpy()
+        # set each column to a numpy array for processing
+        S = df['speed'].to_numpy()
+        p = df['power'].to_numpy()
 
-    neigh_radius = RadiusNeighborsRegressor(radius=1.7, weights='distance', p = 2)
-    neigh_radius.fit(S, p)
+        neigh_radius = RadiusNeighborsRegressor(radius=1.7, weights='distance', p = 2)
+        neigh_radius.fit(S.reshape(-1, 1), p)
 
-    p_pred = neigh_radius.predict(3)
+        p_pred = neigh_radius.predict([[speed]])
 
-    return {'value': p_pred}
+        return {'value': p_pred[0]}
 
 if __name__ == "__main__":
     app.run(debug=True)
